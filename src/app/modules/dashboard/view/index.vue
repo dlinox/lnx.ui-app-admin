008e2ca24d11d263a2910288da843fd65d3df7fe
dlinox <nearlino20@gmail.com>
2025-02-14 08:27:54 -0500

    deploy in new server

--- src/app/modules/dashboard/view/index.vue
+++ src/app/modules/dashboard/view/index.vue
@@ -2,15 +2,15 @@
 
 <div style="display: flex; flex-wrap: wrap; justify-content: center; align-items: center; gap: 10px; margin-top: 20px; margin-bottom: 20px;">
   
-    <div v-for="icon in iconNames" :key="icon" class="icon" style="font-size: 30px; border: 1px solid #000; padding: 10px; ">
+    <!-- <div v-for="icon in iconNames" :key="icon" class="icon" style="font-size: 30px; border: 1px solid #000; padding: 10px; ">
         <span>{{icon}}</span>
         <br>
         <LnxIcon :iconName="icon" ></LnxIcon>
-    </div>
+    </div> -->
 </div>
 
 </template>
 <script setup lang="ts">
-import LnxIcon from '@/core/components/LnxIcon.vue';
-import {iconNames} from '@/core/constans/icons.constants'
+// import LnxIcon from '@/core/components/LnxIcon.vue';
+// import {iconNames} from '@/core/constans/icons.constants'
 </script>
